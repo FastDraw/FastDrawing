a3d5ea186175c450cc3de3905570162ef69590cf
chalvagit <clv.chalva@gmail.com>
2016-11-23 00:01:39 +0100

    added toolbox buttons listeners

--- app/src/main/java/com/company/andrzej/fastdraw/CustomBottomToolbarFragment.java
+++ app/src/main/java/com/company/andrzej/fastdraw/CustomBottomToolbarFragment.java
@@ -8,7 +8,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 import butterknife.ButterKnife;
 
@@ -17,7 +19,7 @@ public class CustomBottomToolbarFragment extends Fragment {
     private Activity activity;
     private Context context;
     private ImageButton btnHide;
-
+    private ToggleButton pencil, pen, marker, color_black, color_red, color_green, color_blue, eraser;
 
     @Override
     public void onAttach(Activity activity) {
@@ -32,7 +34,16 @@ public class CustomBottomToolbarFragment extends Fragment {
         View view = inflater.inflate(R.layout.custom_toolbar_fragment, container, false);
         ButterKnife.bind(this, view);
         btnHide = (ImageButton) view.findViewById(R.id.btn_hidefragment);
+        pencil = (ToggleButton) view.findViewById(R.id.pencil);
+        pen = (ToggleButton) view.findViewById(R.id.pen);
+        marker = (ToggleButton) view.findViewById(R.id.marker);
+        color_black = (ToggleButton) view.findViewById(R.id.color_black);
+        color_red = (ToggleButton) view.findViewById(R.id.color_red);
+        color_green = (ToggleButton) view.findViewById(R.id.color_green);
+        color_blue = (ToggleButton) view.findViewById(R.id.color_blue);
+        eraser = (ToggleButton) view.findViewById(R.id.eraser);
         hideFragment();
+        setButtonsListeners();
         return view;
     }
 
@@ -42,6 +53,144 @@ public class CustomBottomToolbarFragment extends Fragment {
             public void onClick(View v) {
                 ((MainActivity)activity).hideToolbarFragment();
                 ((MainActivity)activity).setButtonsVisible();
+            }
+        });
+    }
+
+    private void setButtonsListeners() {
+        pencil.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // temporary alfa change to indicate button on/off status
+                if (isChecked){
+                    pencil.setAlpha(0.5f);
+                    pen.setAlpha(1f);
+                    marker.setAlpha(1f);
+                    pen.setChecked(false);
+                    marker.setChecked(false);
+                    pencil.setEnabled(false);
+                    pen.setEnabled(true);
+                    marker.setEnabled(true);
+                }
+            }
+        });
+        pen.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // temporary alfa change to indicate button on/off status
+                if (isChecked){
+                    pencil.setAlpha(1f);
+                    pen.setAlpha(0.5f);
+                    marker.setAlpha(1f);
+                    pencil.setChecked(false);
+                    marker.setChecked(false);
+                    pencil.setEnabled(true);
+                    pen.setEnabled(false);
+                    marker.setEnabled(true);
+                }
+            }
+        });
+        marker.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // temporary alfa change to indicate button on/off status
+                if (isChecked){
+                    pencil.setAlpha(1f);
+                    pen.setAlpha(1f);
+                    marker.setAlpha(0.5f);
+                    pencil.setChecked(false);
+                    pen.setChecked(false);
+                    pencil.setEnabled(true);
+                    pen.setEnabled(true);
+                    marker.setEnabled(false);
+                }
+            }
+        });
+        color_black.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // temporary alfa change to indicate button on/off status
+                if (isChecked){
+                    color_black.setAlpha(0.5f);
+                    color_red.setAlpha(1f);
+                    color_green.setAlpha(1f);
+                    color_blue.setAlpha(1f);
+                    color_red.setChecked(false);
+                    color_green.setChecked(false);
+                    color_blue.setChecked(false);
+                    color_black.setEnabled(false);
+                    color_red.setEnabled(true);
+                    color_green.setEnabled(true);
+                    color_blue.setEnabled(true);
+                }
+            }
+        });
+        color_red.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // temporary alfa change to indicate button on/off status
+                if (isChecked){
+                    color_black.setAlpha(1f);
+                    color_red.setAlpha(0.5f);
+                    color_green.setAlpha(1f);
+                    color_blue.setAlpha(1f);
+                    color_black.setChecked(false);
+                    color_green.setChecked(false);
+                    color_blue.setChecked(false);
+                    color_black.setEnabled(true);
+                    color_red.setEnabled(false);
+                    color_green.setEnabled(true);
+                    color_blue.setEnabled(true);
+                }
+            }
+        });
+        color_green.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // temporary alfa change to indicate button on/off status
+                if (isChecked){
+                    color_black.setAlpha(1f);
+                    color_red.setAlpha(1f);
+                    color_green.setAlpha(0.5f);
+                    color_blue.setAlpha(1f);
+                    color_black.setChecked(false);
+                    color_red.setChecked(false);
+                    color_blue.setChecked(false);
+                    color_black.setEnabled(true);
+                    color_red.setEnabled(true);
+                    color_green.setEnabled(false);
+                    color_blue.setEnabled(true);
+                }
+            }
+        });
+        color_blue.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // temporary alfa change to indicate button on/off status
+                if (isChecked){
+                    color_black.setAlpha(1f);
+                    color_red.setAlpha(1f);
+                    color_green.setAlpha(1f);
+                    color_blue.setAlpha(0.5f);
+                    color_black.setChecked(false);
+                    color_red.setChecked(false);
+                    color_green.setChecked(false);
+                    color_black.setEnabled(true);
+                    color_red.setEnabled(true);
+                    color_green.setEnabled(true);
+                    color_blue.setEnabled(false);
+                }
+            }
+        });
+        eraser.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // temporary alfa change to indicate button on/off status
+                if (isChecked){
+                    eraser.setAlpha(0.5f);
+                } else {
+                    eraser.setAlpha(1f);
+                }
             }
         });
     }
